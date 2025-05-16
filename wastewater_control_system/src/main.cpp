e2d967cc9450b96551a12c5a1f1525b235701600
deepsn458 <deepaknarayan458@gmail.com>
2025-05-16 10:13:30 -0500

    changes before changong branch

--- wastewater_control_system/src/main.cpp
+++ wastewater_control_system/src/main.cpp
@@ -16,9 +16,14 @@
 
 #define PRESSURE_LIMIT 100
 float pHVal = 0;
+float condVal = 0;
 int same_read_count = 0;
+int cal_count = 0;
+
 const int PG01_SENSOR_ADDRESS = 114;
 const int PH_SENSOR_ADDRESS_ADJUSTMENT = 102;
+const int COND_ED2_SENSOR_ADDRESS = 0x69;
+const int COND_ED1_SENSOR_ADDRESS = 0x70;
 const int LLS04LowPin = 14;
 const int LLS04HighPin = 35;
 TaskHandle_t monitorSensorsHandle = NULL;
@@ -26,6 +31,9 @@ TaskHandle_t controlPH01Handle = NULL;
 TaskHandle_t controlPH02Handle = NULL;
 Ezo_board pG01Sensor = Ezo_board(PG01_SENSOR_ADDRESS, "pG01");
 Ezo_board pHSensor_adjustment = Ezo_board(PH_SENSOR_ADDRESS_ADJUSTMENT, "PH_ADJ");
+Ezo_board condEd2_sensor = Ezo_board(COND_ED2_SENSOR_ADDRESS, "COND_ED2");
+Ezo_board condEd1_sensor = Ezo_board(COND_ED1_SENSOR_ADDRESS, "COND_ED1");
+
 #define DOSING_PUMP_ADDRESS_1 0x39  // Example address for Tank 1 dosing pump
 Ezo_board dosingPumpTank01 = Ezo_board(DOSING_PUMP_ADDRESS_1, "PMP1");
 /* Using core 1 of ESP32 */
@@ -56,44 +64,55 @@ void setup(){
   Serial.print(WiFi.localIP());
   initFirebase(DATABASE_URL);
 
-  Serial2.print("I2C,114");
-  Serial2.print('\r');
-  Serial.println("Firebase is ready");
-  //xTaskCreatePinnedToCore(controlPH01,"PH01 Control", 6048,NULL,2, &controlPH01Handle, app_cpu);
-  xTaskCreatePinnedToCore(phControl,"PH Control", 10000,NULL,2, &controlPH02Handle, app_cpu);
-  xTaskCreatePinnedToCore(monitorSensors,"Monitor Sensors", 10000, NULL, 2,&monitorSensorsHandle, app_cpu);
-  //pHSensor.send_cmd("Cal,clear");
-  Serial.println("y");
 
 }
 
 
 
 void loop() {
-  Serial2.print("I2C,114");
-  Serial2.print('\r');
+  
+  condEd2_sensor.send_read_cmd();
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  condEd2_sensor.receive_read_cmd();
+  Serial.println(condEd2_sensor.get_last_received_reading());
   /*
-  pHSensor.send_read_cmd();
+  if (cal_count == 0){
+    condEd2_sensor.send_cmd("Cal,clear");
+  }
+  condEd2_sensor.send_read_cmd();
   vTaskDelay(1000 / portTICK_PERIOD_MS);
   // Assume get_reading() returns a String with the sensor's response.
-  pHSensor.receive_read_cmd();
-  Serial.println(pHSensor.get_last_received_reading());
-  if (pHVal - (float)pHSensor.get_last_received_reading()<100){
+  condEd2_sensor.receive_read_cmd();
+  Serial.println(condEd2_sensor.get_last_received_reading());
+  if ((condVal - (float)condEd2_sensor.get_last_received_reading())<1.0){
     same_read_count++;
   }
   else{
     same_read_count = 0;
   }
-  pHVal = pHSensor.get_last_received_reading();
-  if (same_read_count == 3){
-    pHSensor.send_cmd("Cal,84");
+  condVal = condEd2_sensor.get_last_received_reading();
+  Serial.println(condVal);
+  if (same_read_count == 5){
+    if (cal_count == 0){
+      Serial.println("dry cal");
+       condEd2_sensor.send_cmd("Cal,dry");
+       same_read_count = 0;
+    }
+    else if (cal_count == 1){
+      Serial.println("cal 84");
+      condEd2_sensor.send_cmd("Cal,84");
+      same_read_count = 0;
+    }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    pHSensor.send_cmd("Cal,?");
+    condEd2_sensor.send_cmd("Cal,?");
+    
     vTaskDelay(300 / portTICK_PERIOD_MS);
     char cal[15];
-    pHSensor.receive_cmd(cal,15);
+    condEd2_sensor.receive_cmd(cal,15);
     Serial.println(cal);
-
+    vTaskDelay(60000 / portTICK_PERIOD_MS);
+    cal_count++;
+  
   }
   */
   // Keep Firebase tasks running
